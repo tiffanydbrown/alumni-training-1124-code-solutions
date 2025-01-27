4934b408329a3e874e87f1684a2bb9569c475cfd
Tiffany Brown <tdbrown_2001@yahoo.com>
2025-01-27 20:10:26 +0000

    Completed exercise.

--- typescript-operators-and-expressions/main.js
+++ typescript-operators-and-expressions/main.js
@@ -1,39 +1,34 @@
-const width: number = 5;
-const height: number = 8;
-const area: number = width * height;
+'use strict';
+const width = 5;
+const height = 8;
+const area = width * height;
 console.log('Value of Area: ', area);
 console.log('Data Type of Area: ', typeof area);
-
-const bill: number = 75;
-const payment: number = 125;
-const change: number = payment - bill;
+const bill = 75;
+const payment = 125;
+const change = payment - bill;
 console.log('Amount of Change: ', change);
 console.log('Data Type of Change: ', typeof change);
-
-const quizzes: number = 15;
-const midterm: number = 25;
-const final: number = 50;
-const grade: number = (quizzes + midterm + final) / 3;
+const quizzes = 15;
+const midterm = 25;
+const final = 50;
+const grade = (quizzes + midterm + final) / 3;
 console.log('Value of grade: ', grade);
 console.log('Data Type of Grade: ', typeof grade);
-
-const firstName: string = 'Tiffany';
-const lastName: string = 'Brown';
-const fullName: string = firstName + ' ' + lastName;
+const firstName = 'Tiffany';
+const lastName = 'Brown';
+const fullName = firstName + ' ' + lastName;
 console.log('Value of fullName: ', fullName);
 console.log('Data Type of fullName: ', typeof fullName);
-
-const pH: number = 8;
-const isAcidic: boolean = pH > 7;
+const pH = 8;
+const isAcidic = pH > 7;
 console.log('Value of isAcidic: ', isAcidic);
 console.log('Data Type of isAcidic: ', typeof isAcidic);
-
-const headCount: number = 322;
-const isSparta: boolean = headCount === 300;
+const headCount = 322;
+const isSparta = headCount === 300;
 console.log('The value of isSparta: ', isSparta);
 console.log('Data Type of isSparta: ', typeof isSparta);
-
-let motto: string = fullName;
+let motto = fullName;
 motto += ' is the GOAT';
 console.log('The value of motto: ', motto);
 console.log('The Data Type of motto: ', typeof motto);
