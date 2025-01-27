e9dc3bb185c5d820b343313177503cc2c91b7111
Tiffany Brown <tdbrown_2001@yahoo.com>
2025-01-27 20:38:00 +0000

    Completed exercise.

---
+++ typescript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+'use strict';
+const fullName = 'Tiffany Brown';
+const isCool = true;
+const totalPets = 1;
+console.log('Full Name:', fullName);
+console.log('Cool?', isCool);
+console.log('Total # of Pets:', totalPets);
+console.log('Data Type of Full Name:', typeof fullName);
+console.log('Data Type of isCool:', typeof isCool);
+console.log('Data Type of totalPets:', typeof totalPets);
+const empty = null;
+console.log('Empty:', empty);
+console.log('Data Type of empty:', typeof empty);
+let nothing;
+console.log('Nothing:', nothing);
+console.log('Data Type of Nothing:', typeof nothing);
