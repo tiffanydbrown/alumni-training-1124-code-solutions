3620de2983eee63c79d3e9f9411810cd46f7c4ff
Tiffany Brown <tdbrown_2001@yahoo.com>
2025-02-16 00:50:36 +0000

    Completed exercises.

---
+++ typescript-methods/main.ts
@@ -0,0 +1,48 @@
+// The Math Object
+
+const heroes: string[] = ['Iron Man', 'Hulk', 'Wolverine', 'Wonder Woman'];
+let randomNumber: number = Math.random();
+
+randomNumber = randomNumber * heroes.length;
+
+const randomIndex: number = Math.floor(randomNumber);
+console.log('The value of randomIndex:', randomIndex);
+
+const randomHero: string = heroes[randomIndex];
+console.log('Value of randomHero:', randomHero);
+
+// Array Methods
+
+const library: any[] = [
+  {
+    title: 'Pride and Prejudice',
+    author: 'Jane Austen',
+  },
+  {
+    title: 'The Stand',
+    author: 'Stephen King',
+  },
+  {
+    title: 'The Song of Achilles',
+    author: 'Madeline Miller',
+  },
+];
+
+const lastBook: any = library.pop();
+console.log('Value of lastBook:', lastBook);
+const firstBook: any = library.shift();
+console.log('Value of firstBook:', firstBook);
+
+const js = {
+  title: 'JavaScript for Impatient Programmers',
+  author: 'Dr. Axel Rauschmayer',
+};
+const css = {
+  title: 'CSS Secrets',
+  author: 'Lea Verou',
+};
+
+library.push(js);
+library.unshift(css);
+library.splice(1, 1);
+console.log('Value of library object:', library);
