881214a1446bded30b9353df943ff83f6ee85939
Tiffany Brown <tdbrown_2001@yahoo.com>
2025-02-20 20:32:23 +0000

    Completed exercises.

--- typescript-loops/main.js
+++ typescript-loops/main.js
@@ -29,3 +29,52 @@ function getEvenNumbersToTwenty() {
   }
   return evenNumbers;
 }
+function repeatWord(word, times) {
+  let count = 1;
+  let repeated = '';
+  while (count <= times) {
+    repeated += word;
+    count++;
+  }
+  return repeated;
+}
+function logEachCharacter(str) {
+  for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+  }
+}
+function doubleAll(numbers) {
+  const doubled = [];
+  for (let i = 0; i < numbers.length; i++) {
+    doubled.push(numbers[i] * 2);
+  }
+  return doubled;
+}
+function sumArray(numbers) {
+  let sum = 0;
+  for (const num of numbers) {
+    sum += num;
+  }
+  return sum;
+}
+function reverseString(str) {
+  let reversed = '';
+  for (const char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
+}
+function getKeys(obj) {
+  const keys = [];
+  for (const key in obj) {
+    keys.push(key);
+  }
+  return keys;
+}
+function getValues(obj) {
+  const values = [];
+  for (const key in obj) {
+    values.push(obj[key]);
+  }
+  return values;
+}
