fd7d567471e0b8fc76c8bc7efafdc51cd1f47d31
Tiffany Brown <tdbrown_2001@yahoo.com>
2025-02-20 02:55:11 +0000

    Solved two exercises.

--- typescript-loops/main.js
+++ typescript-loops/main.js
@@ -1,3 +1,4 @@
+'use strict';
 /* exported getNumbersToTen,
             getEvenNumbersToTwenty,
             repeatWord,
@@ -8,21 +9,18 @@
             getKeys,
             getValues
  */
-function getNumbersToTen(): any[] {
-  const numbers: any[] = [];
-  let currentNumber: number = 1;
-
+function getNumbersToTen() {
+  const numbers = [];
+  let currentNumber = 1;
   while (currentNumber < 11) {
     numbers.push(currentNumber);
     currentNumber++;
   }
   return numbers;
 }
-
-function getEvenNumbersToTwenty(): any[] {
-  const evenNumbers: any[] = [];
-  let currentNumber: number = 2;
-
+function getEvenNumbersToTwenty() {
+  const evenNumbers = [];
+  let currentNumber = 2;
   while (currentNumber <= 20) {
     if (currentNumber % 2 === 0) {
       evenNumbers.push(currentNumber);
