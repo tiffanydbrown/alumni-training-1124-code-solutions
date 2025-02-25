1f8a8db3697722d6d267b633e8cd9c3d1ade4b00
Tiffany Brown <tdbrown_2001@yahoo.com>
2025-02-25 03:07:59 +0000

    Completed exercises.

--- typescript-loops-practice/filter-out-nulls.js
+++ typescript-loops-practice/filter-out-nulls.js
@@ -1,2 +1,11 @@
 'use strict';
 /* exported filterOutNulls */
+function filterOutNulls(values) {
+  const arr = [];
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] !== null) {
+      arr.push(values[i]);
+    }
+  }
+  return arr;
+}
--- typescript-loops-practice/filter-out-nulls.ts
+++ typescript-loops-practice/filter-out-nulls.ts
@@ -1,1 +1,11 @@
 /* exported filterOutNulls */
+function filterOutNulls(values: any[]): any[] {
+  const arr: any[] = [];
+
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] !== null) {
+      arr.push(values[i]);
+    }
+  }
+  return arr;
+}
