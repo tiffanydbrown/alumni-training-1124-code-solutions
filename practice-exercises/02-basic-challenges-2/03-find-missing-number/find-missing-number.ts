adcfe54a839e8e0b9d2e4b2c8a4e2b0259272d81
Tiffany Brown <tdbrown_2001@yahoo.com>
2025-04-24 01:28:00 +0000

    Completed two more exercises in Basic Challenges 2.

--- practice-exercises/02-basic-challenges-2/03-find-missing-number/find-missing-number.ts
+++ practice-exercises/02-basic-challenges-2/03-find-missing-number/find-missing-number.ts
@@ -1,3 +1,11 @@
 export function findMissingNumber(arr: number[]): number | undefined {
-  return undefined;
+  let sum: number = 0;
+  const n: number = arr.length + 1;
+  const inclusive: number = (n * (n + 1)) / 2;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  const missNum = inclusive - sum;
+  return missNum;
 }
