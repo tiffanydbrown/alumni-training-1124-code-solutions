41b98d9982901485e647ce0038bdb057fd5b40f4
Tiffany Brown <tdbrown_2001@yahoo.com>
2025-04-26 20:47:50 +0000

    Completed a couple exercises..

--- practice-exercises/02-basic-challenges-2/08-format-phone-number/format-phone-number.ts
+++ practice-exercises/02-basic-challenges-2/08-format-phone-number/format-phone-number.ts
@@ -1,3 +1,7 @@
 export function formatPhoneNumber(numbers: number[]): string {
-  return '';
+  const areaCode = numbers.slice(0, 3).join('');
+  const prefix = numbers.slice(3, 6).join('');
+  const lineNum = numbers.slice(6).join('');
+
+  return `(${areaCode}) ${prefix}-${lineNum}`;
 }
