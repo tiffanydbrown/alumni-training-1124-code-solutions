5e922be1c2c5ce1d2de189ac2dd92c250af47ce5
Tiffany Brown <tdbrown_2001@yahoo.com>
2025-04-28 21:53:40 +0000

    Completed sum of even squares activity.

--- practice-exercises/03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.ts
+++ practice-exercises/03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.ts
@@ -1,3 +1,15 @@
 export function sumOfEvenSquares(numbers: number[]): number {
-  return NaN;
+  const filterNumbers = numbers.filter((num) => {
+    return num % 2 === 0;
+  });
+
+  const mapNumbers = filterNumbers.map((num) => {
+    return num ** 2;
+  });
+
+  const sum = mapNumbers.reduce((total, num) => {
+    return total + num;
+  }, 0);
+
+  return sum;
 }
