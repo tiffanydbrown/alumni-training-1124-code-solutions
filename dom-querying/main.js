cfff0956aad7554ce40b87df66a35e80a3442cf9
Tiffany Brown <tdbrown_2001@yahoo.com>
2025-04-28 23:20:15 +0000

    Completed activity.

---
+++ dom-querying/main.js
@@ -0,0 +1,15 @@
+'use strict';
+console.log('hello world');
+const $h1 = document.querySelector('h1');
+console.log($h1);
+console.dir($h1);
+const $explanation = document.querySelector('#explanation');
+console.log($explanation);
+console.dir($explanation);
+const $hint = document.querySelector('.hint');
+console.log($hint);
+console.dir($hint);
+const $pQuery = document.querySelectorAll('p');
+console.log($pQuery);
+const $exampleLink = document.querySelectorAll('.example-link');
+console.log($exampleLink);
