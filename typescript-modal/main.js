674c3ebe0e9505d4e9d04b2aafc64f55121752c2
Tiffany Brown <tdbrown_2001@yahoo.com>
2025-05-05 23:27:18 +0000

    Completed modal activity.

--- typescript-modal/main.js
+++ typescript-modal/main.js
@@ -1,20 +1,15 @@
+'use strict';
 const $openModal = document.querySelector('.open-modal');
 if (!$openModal) throw new Error('open-model does not exist');
-
-function handleClick(): void {
+function handleClick(event) {
   $dialog?.showModal();
 }
-
 $openModal.addEventListener('click', handleClick);
-
 const $dismissModal = document.querySelector('.dismiss-modal');
 if (!$dismissModal) throw new Error('dismiss-modal does not exist');
-
-function handleClickClose(): void {
+function handleClickClose(event) {
   $dialog?.close();
 }
-
 $dismissModal.addEventListener('click', handleClickClose);
-
 const $dialog = document.querySelector('dialog');
 if (!$dialog) throw new Error('dialog does not exist');
